ffc41afa99cffb78a9ea25a8a10933e2b97d99eb
Anna Perlack <annaperlack@gmail.com>
2023-01-09 22:54:33 -0500

    added manager prompt to index.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,4 +1,21 @@
-const Employee = 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+const Employee = require('../lib/Employee')
+
+test('test adding id to employee', () => {
+    const person = new Employee(1, 'Anna', 'annaperlack@gmail.com')
+    expect(person.id).toBe(1);
+  });
+
+  test('test adding name to employee', () => {
+    const person = new Employee(1, 'Anna', 'annaperlack@gmail.com')
+    expect(person.name).toBe('Anna');
+  });
+
+  test('test adding email to employee', () => {
+    const person = new Employee(1, 'Anna', 'annaperlack@gmail.com')
+    expect(person.email).toBe('annaperlack@gmail.com');
+  });
+
+  test('test adding getid method returns id', () => {
+    const person = new Employee(1, 'Anna', 'annaperlack@gmail.com')
+    expect(person.getid()).toBe(1);
   });
---
+++ index.js
@@ -0,0 +1,37 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+let employees = []
+
+function addManager() {
+    inquirer
+       .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of your maanger',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the id of your maanger',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the email of your maanger',
+            },
+            {
+                type: 'input',
+                name: 'number',
+                message: 'What is the office number of your maanger',
+            },
+       ])
+       .then((answers) => {
+        const manager = new Manager(answers.id, answers.name, answers.email, answers.number)
+        employees.push(manager)
+        //trigger function that asks my user if they want to add another employee or are they ready to build the team?
+       })
+}
