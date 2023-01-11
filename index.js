5dd944a94c02e7d60a211ea5a5fc9b1ceb87f155
Anna Perlack <annaperlack@gmail.com>
2023-01-10 20:53:23 -0500

    added prompts to js file

--- index.js
+++ index.js
@@ -3,36 +3,116 @@ const fs = require('fs');
 const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern')
 let employees = []
 
-function addManager() {
     inquirer
        .prompt([
             {
                 type: 'input',
                 name: 'name',
-                message: 'What is the name of your maanger',
+                message: 'What is the name of the manager?',
             },
             {
                 type: 'input',
                 name: 'id',
-                message: 'What is the id of your manager',
+                message: 'What is the id of the manager?',
             },
             {
                 type: 'input',
                 name: 'email',
-                message: 'What is the email of your manager',
+                message: 'What is the email of the manager?',
             },
             {
                 type: 'input',
                 name: 'number',
-                message: 'What is the office number of your maanger',
+                message: 'What is the office number of the manager?',
             },
        ])
        .then((answers) => {
         const manager = new Manager(answers.id, answers.name, answers.email, answers.number)
         employees.push(manager)
-       
-        //trigger function that asks my user if they want to add another employee or are they ready to build the team?
+        console.log(answers.id, answers.name, answers.email, answers.number)
+        promptNext()
        })
-}
+
+       function promptNext(){
+        inquirer
+            .prompt([
+            {
+                type: 'list',
+                name: 'choices',
+                message: 'What do you want to do next?',
+                choices: [
+                    {name: 'add an engineer', value: 'engineer'},
+                    {name: 'add an intern', value: 'intern'},
+                    {name: 'complete team profile', value: 'done'},
+                ],
+            },
+
+       ])
+       .then((answers) => {
+        if (answers.choices == 'engineer') {
+            inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'name',
+                    message: 'What is the name of the engineer?',
+                }, 
+                {
+                    type: 'input',
+                    name: 'name',
+                    message: 'What is the id of the engineer?'
+                },
+                {
+                    type: 'input',
+                    name: 'email',
+                    message: 'What is the email of the engineer?',
+                },
+                {
+                    type: 'input',
+                    name: 'number',
+                    message: 'What is the github username of the engineer?',
+                },
+            ])
+            .then((answers) => {
+                const newEngineer = new Engineer(answers)
+                employees.push(newEngineer)
+            })
+       }  else if (answers.choices == 'intern') {
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of the intern?',
+            }, 
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the id of the intern?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the email of the intern?',
+            },
+            {
+                type: 'input',
+                name: 'number',
+                message: 'What school did the intern attend?',
+            },
+        ])
+        .then((answers) => {
+            const newIntern = new Intern(answers)
+            employees.push(newIntern)
+        })} 
+        else if (answers.choices == 'done') {
+        }
+        
+       })
+
+       
+    }
+    
